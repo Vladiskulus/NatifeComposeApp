005d3c6ef82ae3da421efa69d1d6aa9bfc2f0dee
Vladiskulum <94043171+Vladiskulum@users.noreply.github.com>
2023-09-26 13:51:23 +0300

    1. Isolate screen elements

--- app/src/main/java/ua/vn/iambulance/natifeapp/data/paging/GiphyPagingSource.kt
+++ app/src/main/java/ua/vn/iambulance/natifeapp/data/paging/GiphyPagingSource.kt
@@ -3,9 +3,7 @@ package ua.vn.iambulance.natifeapp.data.paging
 import android.util.Log
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import ua.vn.iambulance.natifeapp.data.API_KEY
-import ua.vn.iambulance.natifeapp.data.LIMIT
-import ua.vn.iambulance.natifeapp.data.REQUEST
+import ua.vn.iambulance.natifeapp.data.*
 import ua.vn.iambulance.natifeapp.data.entity.GiphyData
 import ua.vn.iambulance.natifeapp.data.retrofit.ApiService
 
@@ -21,12 +19,14 @@ class GiphyPagingSource(private val apiService: ApiService):PagingSource<Int, Gi
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, GiphyData> {
         return try {
             val page = params.key ?: 1
-            val response = apiService.getTrendingGifs(API_KEY, LIMIT, page * LIMIT, REQUEST, "messaging_non_clips")
+            val size = params.loadSize
+            val from = page * size
+            val response = apiService.getTrendingGifs(API_KEY, size, from, REQUEST, "messaging_non_clips")
             Log.d("RESPONSE","$page and ${page * LIMIT}" )
             LoadResult.Page(
                 data = response.data,
                 prevKey = if (page == 1) null else page.minus(1),
-                nextKey = if (response.data.isEmpty()) null else page.plus(1),
+                nextKey = if (response.data.isEmpty()) null else page.plus(1)
             )
         } catch (e: Exception) {
             LoadResult.Error(e)
